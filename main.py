632b50e1cacb5d345190942c086f53a204f039e1
Pave1C0de <pave1.p0st@yandex.ru>
2022-06-15 21:19:25 +0300

    hw5

---
+++ main.py
@@ -0,0 +1,24 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+###########################################
+#pip install django
+#python3 -m django --version
+#python3 -m django startproject geekshop
+#cd geekshop
+#python3 -m django startapp mainapp
+
+#python3 manage.py runserver
+###########################################
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
